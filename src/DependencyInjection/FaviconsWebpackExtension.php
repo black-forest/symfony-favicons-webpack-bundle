c38b614eb12e78fc6c067f4a36b024ce6b2934b8
Sven Baumann <baumann.sv@gmail.com>
2021-05-04 18:35:06 +0200

    Configure the favicons definition

--- src/DependencyInjection/FaviconsWebpackExtension.php
+++ src/DependencyInjection/FaviconsWebpackExtension.php
@@ -21,9 +21,12 @@ declare(strict_types=1);
 
 namespace BlackForest\Symfony\WebpackEncoreBundle\DependencyInjection;
 
+use BlackForest\Symfony\WebpackEncoreBundle\Asset\Favicons;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -41,5 +44,56 @@ final class FaviconsWebpackExtension extends Extension
 
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
+
+        $this->configureFavicons($config, $container);
+    }
+
+    /**
+     * Configure favicons.
+     *
+     * @param array            $config    The bundle configuration.
+     * @param ContainerBuilder $container The container builder.
+     *
+     * @return void
+     */
+    private function configureFavicons(array $config, ContainerBuilder $container): void
+    {
+        if (!isset($config['favicons']) || empty($config['favicons'])) {
+            return;
+        }
+
+        $factories = [];
+        $cacheKeys = [];
+        foreach ($config['favicons'] as $name => $path) {
+            $factories[$name] = $this->faviconsFactory($container, $config['cache'], $name, $path);
+            $cacheKeys[$name] = $path;
+        }
+    }
+
+    /**
+     * Create the favicons definition.
+     *
+     * @param ContainerBuilder $container The container builder.
+     * @param bool             $useCache  Determine for use the cache.
+     * @param string           $name      The favicon name.
+     * @param string           $path      The favicon path.
+     *
+     * @return Reference
+     */
+    private function faviconsFactory(ContainerBuilder $container, bool $useCache, string $name, string $path): Reference
+    {
+        $id        = \sprintf('%s[%s]', Favicons::class, $name);
+        $arguments = [
+            '$faviconHtmlPath' => $path,
+            '$cacheKey'        => $name,
+            '$fileLocator'     => new Reference('file_locator'),
+            '$cache'           => $useCache ? new Reference('favicons_webpack.cache') : null
+        ];
+
+        $definition = new Definition(Favicons::class, $arguments);
+        $definition->addTag('kernel.reset', ['method' => 'reset']);
+        $container->setDefinition($id, $definition);
+
+        return new Reference($id);
     }
 }
