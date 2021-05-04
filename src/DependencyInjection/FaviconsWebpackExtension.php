0fb6b0b4a3be931b2c82a68b3e9c878b47b30cae
Sven Baumann <baumann.sv@gmail.com>
2021-05-04 17:33:33 +0200

    Add the extension

---
+++ src/DependencyInjection/FaviconsWebpackExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of blackforest/symfony-favicons-webpack-bundle.
+ *
+ * (c) 2014-2021 The Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    blackforest/symfony-favicons-webpack-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2021 The Blackforest team.
+ * @license    https://github.com/blackforest/symfony-favicons-webpack-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+declare(strict_types=1);
+
+namespace BlackForest\Symfony\WebpackEncoreBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * The favicons webpack extension.
+ */
+final class FaviconsWebpackExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        // TODO: Implement load() method.
+    }
+}
